8cce1bfa471a797faa744ee8e17cb4f81542eb6d
Fuat17h <fuat17h@gmail.com>
2022-10-24 10:32:29 +0300

    my first commit

---
+++ Service/CompanyService.cs
@@ -0,0 +1,49 @@
+﻿using Contracts;
+using Shared.DataTransferObjects;
+using Service.Contracts;
+using AutoMapper;
+using Entities.Exceptions;
+
+namespace Service
+{
+    internal sealed class CompanyService : ICompanyService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
+        {
+            //try
+            //{
+                var companies =  _repository.Company.GetAllCompanies(trackChanges);
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+                return companiesDto;
+            //}
+            //catch(Exception ex)
+            //{
+            //    _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
+            //    throw;
+            //}
+        }
+
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges);
+            //check if the company is null
+
+            if (company is null)
+                throw new CompanyNotFoundException(id);
+
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return companyDto;
+        }
+
+    }
+}
---
+++ Service/EmployeeService.cs
@@ -0,0 +1,62 @@
+﻿using Contracts;
+using Service.Contracts;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.DataTransferObjects;
+using Entities.Exceptions;
+using Entities.Models;
+
+namespace Service
+{
+    internal sealed class EmployeeService : IEmployeeService
+    {
+
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+            return employeesDto;
+
+            // fetch company entity from DB
+            //if not exist, return NotFound response
+            //if exist, fetch all employee from DB
+            // map them to the collection of EmployeeDto
+            //return to the caller
+        }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+            if (employeeDb is null)
+                throw new EmployeeNotFoundException(id);
+
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+
+            return employee;
+        }
+
+    }
+}
